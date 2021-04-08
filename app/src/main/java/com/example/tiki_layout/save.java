be3c9ae1c719ef72255ca715873e450f2f85c5cd
unknown <buithanhnam26092000@gmail.com>
2021-04-08 21:07:40 +0700

    Lab4

---
+++ app/src/main/java/com/example/tiki_layout/save.java
@@ -0,0 +1,14 @@
+package com.example.tiki_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class save extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.save);
+    }
+}
